03dd4719b5a6a1e1954be1595d08a246b50650fc
dachi <dachi.laferashvili@gmail.com>
2025-03-13 23:26:10 +0400

    feat: get statuses from api

---
+++ script.js
@@ -0,0 +1,21 @@
+// Getting task statuses from momentum api
+async function getStatuses() {
+  try {
+    const res = await fetch(
+      "https://momentum.redberryinternship.ge/api/statuses",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const statuses = await res.json();
+    console.log(statuses);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+getStatuses();
