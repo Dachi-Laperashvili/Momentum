2638414372a208604f6f2445f7b37548f66a9683
dachi <dachi.laferashvili@gmail.com>
2025-03-13 23:49:14 +0400

    feat: displaying statuses

--- script.js
+++ script.js
@@ -13,9 +13,29 @@ async function getStatuses() {
 
     const statuses = await res.json();
     console.log(statuses);
+
+    return statuses;
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
 getStatuses();
+
+async function displayStatusDivs() {
+  try {
+    const statuses = await getStatuses();
+    const container = document.querySelector(".tasks-container");
+
+    statuses.forEach((status, index) => {
+      const div = document.createElement("div");
+      div.classList.add(`status-${index}`);
+      div.textContent = status.name;
+      container.appendChild(div);
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+displayStatusDivs();
